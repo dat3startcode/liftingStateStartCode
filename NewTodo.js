571a5f4e9300e381ae4f329a2392e5ec1b7f0a38
Lars-m <lam@cphbusiness.dk>
2019-10-23 16:37:02 +0200

    Update NewTodo.js

--- NewTodo.js
+++ NewTodo.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 
-const TodoList = ({ todos }) => {
-  return (
-    <React.Fragment>
-      <h2>All Todos</h2>
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>{todo.todoText}</li>
-        ))}
-      </ul>
-    </React.Fragment>
-  );
-};
-export default TodoList;
+function NewTodo(props) {
 
-TodoList.propTypes = {
-  todos: PropTypes.array
+  const [todo, setTodo] = useState(props.nextTodo);
+
+  const handleSubmit = evt => {
+    if (todo.todoText === "") {
+      return;
+    }
+    props.addTodo(todo);
+    evt.preventDefault();
+  };
+
+  useEffect(() => setTodo({ ...props.nextTodo }), [props.nextTodo]);
+
+  const onChange = evt => {
+    const val = evt.target.value;
+    todo.todoText = val;
+    setTodo({ ...todo });
+  };
+  const title = todo.id==="" ?"Create new Todo" : "Edit Todo"
+  return (
+    <div>
+      <h4>{title}</h4>
+      <form onSubmit={handleSubmit}>
+        <input value={todo.todoText} onChange={onChange} />
+        <div style={{ marginTop: 5 }}>
+          <button className="btn btn-info">Save</button>
+        </div>
+      </form>
+      {todo.id !== "" && <p>Editing todo with</p>}
+    </div>
+  );
+}
+export default NewTodo;
+
+NewTodo.propTypes = {
+  nextTodo: PropTypes.object,
+  addTodo: PropTypes.func
 }
